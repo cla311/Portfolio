5c59e9795e024401d3eaea72811b3ad3ba6af097
Christopher Louie <christopherlouie@shaw.ca>
2021-08-25 06:14:12 -0700

    Edit css and seperate common elements to layout fragment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portfolio
-Custom coded portfolio using Java Spring Boot. Based on my original portfolio which originally used a static Bootstrap 4 template.
+Custom coded portfolio using Java Spring Boot and Bootstrap 5.
+
+Based on my original portfolio which originally used a static Bootstrap 4 template.
--- src/main/java/com/cla311/portfolio/models/Project.java
+++ src/main/java/com/cla311/portfolio/models/Project.java
@@ -5,34 +5,23 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity @Table(name = "projects") public class Project {
-	@NotNull @Getter @Setter private String title;
+@Data @NoArgsConstructor @AllArgsConstructor @Entity @Table(name =
+"projects") public class Project {
+	@NotNull private String title;
 
-	@NotNull @Getter @Setter private String image;
+	@NotNull private String image;
 
-	@NotNull @Getter @Setter private String tags;
+	@NotNull private String tags;
 
-	@NotNull @Getter @Setter private String cssTags;
+	@NotNull private String cssTags;
 
-	@NotNull @Getter @Setter private String startDate;
+	@NotNull private String startDate;
 
-	@Getter @Setter private String endDate;
+	private String endDate;
 
-	@Id @Getter @Setter private String link;
-
-	public Project() {}
-
-	public Project(String title, String image, String tags, String cssTags,
-		String startDate, String endDate, String link) {
-		this.title = title;
-		this.image = image;
-		this.tags = tags;
-		this.cssTags = cssTags;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.link = link;
-	}
+	@Id private String link;
 }
