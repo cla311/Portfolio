6c7359926cb8c2cb752a667422acc2ddd56572ff
Christopher Louie <christopherlouie@shaw.ca>
2022-01-23 02:42:31 -0800

    Update tests and resume

---
+++ src/test/java/com/cmpt276/gameinn/PortfolioTests.java
@@ -0,0 +1,15 @@
+package com.cla311.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest public class PortfolioTests {
+	@Autowired private Portfolio controller;
+
+	@Test public void contextLoads()
+	throws Exception {
+		assertThat(controller).isNotNull();
+	}
+}
