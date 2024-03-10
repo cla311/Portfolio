26fdf62aff5e9e8fd23e3e512cf311b639640dd8
Christopher Louie <christopherlouie@shaw.ca>
2024-03-10 06:36:50 -0700

    Fix application context load test to only test the frontend

--- src/test/java/com/cla311/portfolio/PortfolioTests.java
+++ src/test/java/com/cla311/portfolio/PortfolioTests.java
@@ -4,13 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-@SpringBootTest public class PortfolioTests {
-	@Autowired private Portfolio controller;
+@WebMvcTest(Portfolio.class)
+public class PortfolioTests {
+	@Autowired
+	private Portfolio controller;
 
-	@Test public void contextLoads()
-	throws Exception {
+	@Test
+	public void contextLoads()
+			throws Exception {
 		assertThat(controller).isNotNull();
 	}
 }
