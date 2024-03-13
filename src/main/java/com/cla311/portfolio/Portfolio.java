8fd4ac3dfdb70e93dfad208ee04c9306097f16fd
Christopher Louie <christopherlouie@shaw.ca>
2024-03-12 23:01:26 -0700

    Enable Etag filtering to replace cached files when updated

--- src/main/java/com/cla311/portfolio/Portfolio.java
+++ src/main/java/com/cla311/portfolio/Portfolio.java
@@ -3,12 +3,19 @@ package com.cla311.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Controller
 @SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 public class Portfolio {
 	public static void main(String[] args) {
 		SpringApplication.run(Portfolio.class, args);
+	}
+
+	@Bean
+	ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
